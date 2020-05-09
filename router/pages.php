60faf384e829f24cf8f1e2c11c1a6e56482c64e3
Güney Saramali <guneysaramali@live.co.uk>
2020-05-09 23:28:56 +0300

    Update pages.php

--- router/pages.php
+++ router/pages.php
@@ -5,24 +5,25 @@ class pages{
 	var $domain;
 	function __construct(){
 		
-		//Name of the Domain carefull for the http or https
+		//Name of the Domain carefull for the HTTP or HTTPS
 		$this->domain = 'http://yourdomain.com';
 		
 		//Define your pages in here for each language
 		$this->load = array(
-		'home.php' => array('anasayfa','home'),
-		'about.php' => array('hakkimizda','about'),
-		'contact.php' => array('iletisim', 'contact'),
-		'service.php' => array('servisler','services'),
-		'products.php' => array('urunler','products'),
-		'faq.php' => array('sss','faq'),
-		'calculator.php' => array('fiyat','calculator'),
-		'404.php' => array('404'));
+			'home.php' => array('anasayfa','home'),
+			'about.php' => array('hakkimizda','about'),
+			'contact.php' => array('iletisim', 'contact'),
+			'service.php' => array('servisler','services'),
+			'products.php' => array('urunler','products'),
+			'faq.php' => array('sss','faq'),
+			'calculator.php' => array('fiyat','calculator'),
+			'404.php' => array('404')
+		);
 		
 		//Define your default home page name for each language here
 		$this->defaulthome = array(
-		'tr' => 'anasayfa',
-		'en' => 'home'
+			'tr' => 'anasayfa',
+			'en' => 'home'
 		);
 		
 	}
